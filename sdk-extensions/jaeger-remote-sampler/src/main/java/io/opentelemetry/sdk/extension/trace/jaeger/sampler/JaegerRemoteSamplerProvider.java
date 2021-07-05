77bbcc1e28ce1a8fd733be8168d5e0ec0e838672
Pavol Loffay <p.loffay@gmail.com>
2021-07-05 10:44:36 +0200

    Use documented variables
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- sdk-extensions/jaeger-remote-sampler/src/main/java/io/opentelemetry/sdk/extension/trace/jaeger/sampler/JaegerRemoteSamplerProvider.java
+++ sdk-extensions/jaeger-remote-sampler/src/main/java/io/opentelemetry/sdk/extension/trace/jaeger/sampler/JaegerRemoteSamplerProvider.java
@@ -8,29 +8,20 @@ package io.opentelemetry.sdk.extension.trace.jaeger.sampler;
 import io.opentelemetry.sdk.autoconfigure.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigurableSamplerProvider;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
-import java.util.concurrent.TimeUnit;
 
 public class JaegerRemoteSamplerProvider implements ConfigurableSamplerProvider {
 
   @Override
   public Sampler createSampler(ConfigProperties config) {
-    String serviceName = config.getString("jaeger.service");
+    String serviceName = config.getString("otel.service.name");
     JaegerRemoteSamplerBuilder builder = JaegerRemoteSampler.builder().setServiceName(serviceName);
 
     // Optional configuration
-    String endpoint = config.getString("jaeger.remote.sampler.endpoint");
+    String endpoint = config.getString("otel.exporter.jaeger.endpoint");
     if (endpoint != null) {
       builder.setEndpoint(endpoint);
     }
-    Double initialRate = config.getDouble("jaeger.remote.sampler.initial.rate");
-    if (initialRate != null) {
-      builder.setInitialSampler(Sampler.parentBased(Sampler.traceIdRatioBased(initialRate)));
-    }
-    Integer poolingInterval = config.getInt("jaeger.remote.sampler.pooling.interval");
-    if (poolingInterval != null) {
-      builder.setPollingInterval(poolingInterval, TimeUnit.MILLISECONDS);
-    }
-
+    // TODO (pavolloffay) add initial rate and pooling interval once they are added to the spec.
     return builder.build();
   }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -70,7 +70,6 @@ include(":sdk-extensions:tracing-incubator")
 include(":sdk-extensions:jaeger-remote-sampler")
 include(":sdk-extensions:jfr-events")
 include(":sdk-extensions:zpages")
-include(":sdk-extensions:jaeger-remote-sampler")
 
 val isCI = System.getenv("CI") != null
 gradleEnterprise {
